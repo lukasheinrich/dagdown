add14453cf623e4dd6274d4e8e09e3fd60cc8232
lukasheinrich <lukas.heinrich@gmail.com>
2016-11-14 17:18:49 +0100

    dagdown init

---
+++ README.md
@@ -0,0 +1,7 @@
+# DAGdown
+
+short hand notation to define yadage workflows. DAGdown is to yadage workflows as Markdown to HTML.
+
+
+    cat test.dd | python convert.py > workflow.yml
+    yadage-run work workflow.yml -p nevents=100
---
+++ convert.py
@@ -0,0 +1,81 @@
+import yaml
+import string
+
+def parsepar(v):
+    deps = []
+    if '$' in v:
+        stages,output = v.split('|')
+        stages = stages.replace('$','')
+        deps += stages.split(',')
+        return deps,{'stages':stages, 'output':output, 'unwrap':True}
+    else:
+        return deps,v
+
+def parse_parameters(minspec):
+    pars = minspec[1]
+    full_parameters = {}
+    alldeps = []
+    for k,v in pars.iteritems():
+        deps,v = parsepar(v)
+        full_parameters[k] = v
+        alldeps += deps
+    alldeps = list(set(alldeps))
+    return alldeps,full_parameters
+
+def make_simple_step(minspec):
+    environment,cmd = minspec[0].items()[0]
+    image_tag = environment.split(':')
+    if(len(image_tag)==2):
+        image,tag = image_tag
+    else:
+        image,tag = image_tag[0],'latest'
+    fmt = string.Formatter()
+    fields = {name:fmt for _,name,fmt,_ in fmt.parse(cmd)}
+    outputmap = {}
+    for k,v in fields.iteritems():
+        if '>>' in v:
+            cmd = cmd.replace('{}:{}'.format(k,v),k)
+            key = v.split('>>')[1] if v.split('>>')[1] else k
+            outputmap[key] = k
+    return {
+        'process':{
+            'process_type':'string-interpolated-cmd',
+            'cmd':cmd
+        },
+        'publisher':{
+            'publisher_type':'frompar-pub',
+            'outputmap':outputmap
+        },
+        'environment':{
+            'environment_type':'docker-encapsulated',
+            'image':image,
+            'imagetag':tag
+        }
+    }
+
+def make_stage(name,step,pars,deps):
+    return {
+        'name':name,
+        'dependencies':deps,
+        'scheduler':{
+            'scheduler_type':'singlestep-stage',
+            'parameters':pars,
+            'step':step
+        }
+    }
+
+import sys
+import json
+
+def parse_data(data):
+    stages = []
+    for k,v in data.iteritems():
+        step = make_simple_step(v)
+        deps, pars = parse_parameters(v)
+        stage = make_stage(k,step,pars,deps)
+        stages.append(stage)
+    sys.stdout.write(yaml.safe_dump({'stages':stages}, default_flow_style = False))
+
+
+data = yaml.load(sys.stdin.read())
+parse_data(data)
